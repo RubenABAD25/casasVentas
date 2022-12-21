b2fa56b867befe10306dbf5cf88b90463036e951
Frank Montalvo Ochoa <fgmo.dev@fmontalvoo.com>
2022-12-21 13:32:33 -0500

    Se configura swiper para que inicie la animacion automaticamente.
    Signed-off-by: Frank Montalvo Ochoa <fgmo.dev@fmontalvoo.com>

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,18 +1,39 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
 import { Product } from 'src/app/models/product';
+
+import { SwiperComponent } from 'swiper/angular';
+import SwiperCore, { Autoplay, SwiperOptions } from 'swiper';
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss']
 })
-export class ProductCardComponent implements OnInit {
+export class ProductCardComponent implements AfterViewInit {
 
   @Input() product!: Product;
+  @ViewChild('swiperSlideShow') swiperSlideShow!: SwiperComponent;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  config: SwiperOptions = {};
+
+  constructor() {
+    SwiperCore.use([Autoplay]);
+  }
+
+  ngAfterViewInit(): void {
+    this.config = {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      // navigation: true,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+      },
+      // pagination: { clickable: true },
+      // scrollbar: { draggable: true },
+    }
+    this.swiperSlideShow.swiperRef.autoplay.start();
   }
 
 }
